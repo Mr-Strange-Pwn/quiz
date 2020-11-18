0edf20c42691ef6859504f1ff527afff7c25e00b
pawan <pawan.tiwari9411@gmail.com>
2020-11-18 19:12:14 +0530

    Updates

---
+++ precache-manifest.bafff1a9d9832ebe7bcfa2e23e24abba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e1c9a01931b4e9e396b2b7bb4b085b",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "3aa92548de86343ab337",
+    "url": "/quiz/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2e18c1d32538e2840b85",
+    "url": "/quiz/static/css/main.e3ecfbd8.chunk.css"
+  },
+  {
+    "revision": "3aa92548de86343ab337",
+    "url": "/quiz/static/js/2.3756c3a5.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/quiz/static/js/2.3756c3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e18c1d32538e2840b85",
+    "url": "/quiz/static/js/main.20519c1a.chunk.js"
+  },
+  {
+    "revision": "f1d44162bcd58b87e527",
+    "url": "/quiz/static/js/runtime-main.af4706f3.js"
+  }
+]);
